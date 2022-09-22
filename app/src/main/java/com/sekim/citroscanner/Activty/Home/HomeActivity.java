fef2610ecf16943ed3b82fcedb60b1141a534149
sekim@dtwocorp.com <sekim@dtwocorp.com>
2022-09-22 10:24:23 +0900

    기본 화면 생성

---
+++ app/src/main/java/com/sekim/citroscanner/Activty/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.sekim.citroscanner.Activty.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sekim.citroscanner.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/sekim/citroscanner/Activty/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.sekim.citroscanner.Activty.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sekim.citroscanner.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
