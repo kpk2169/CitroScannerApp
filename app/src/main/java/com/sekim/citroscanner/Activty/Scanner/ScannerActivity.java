9597dabc8781948b8101de4c352e769eaec22a8b
sekim@dtwocorp.com <sekim@dtwocorp.com>
2022-09-23 14:26:41 +0900

    임시 저장

--- app/src/main/java/com/sekim/citroscanner/Activty/Scanner/ScannerActivity.java
+++ app/src/main/java/com/sekim/citroscanner/Activty/Scanner/ScannerActivity.java
@@ -1,10 +1,15 @@
 package com.sekim.citroscanner.Activty.Scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sekim.citroscanner.R;
@@ -13,8 +18,12 @@ public class ScannerActivity extends AppCompatActivity {
 
     private final String TAG = "SCANNER";
     private final String PRODUCT = "products";
-    private final String Receipt = "orders";
+    private final String RECEIPT = "orders";
     private String mode;
+
+    private Button btnNow;
+    private ConstraintLayout clBtnChangeMode;
+    private TextView tvMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class ScannerActivity extends AppCompatActivity {
 
         getIntentParams();
         findViewById();
+
+
     }
 
     private void getIntentParams(){
@@ -37,6 +48,21 @@ public class ScannerActivity extends AppCompatActivity {
 
     private void findViewById(){
         try{
+            btnNow = findViewById(R.id.btn_now_mode);
+            btnNow.setEnabled(false);
+
+
+
+            clBtnChangeMode = findViewById(R.id.cl_btn_exchange);
+            tvMode = findViewById(R.id.tv_chage_mode);
+
+            if( mode.equals(PRODUCT)){
+
+            }else if ( mode.equals(RECEIPT)){
+
+            }
+
+            btnNow.setBackgroundTintList( ColorStateList.valueOf(ContextCompat.getColor( getApplicationContext(), R.color.citro_grey ))  );
 
 
 
